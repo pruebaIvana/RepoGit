8773050e08d83f6543bf54f1fab7b5dde75f526a
icmiaton <icmiaton@gmail.com>
2020-06-25 18:31:47 -0300

    geenro un conflcto desde issue1

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,7 +2,7 @@
 public class HelloGit {
 
 	public static void main(String[] args) {
-		String x = "Escribo en issue 1 para generar conflicto";
+		String x = "Escribo en issue 1 para generar conflicto 2";
 
 	}
 
